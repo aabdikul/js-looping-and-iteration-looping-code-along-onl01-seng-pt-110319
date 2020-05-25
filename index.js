0204ccc453a78302069c54bad97f4e24bda505be
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-05-25 01:16:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(friends, surprise) {
+	let array = []
+	for (let i=0; i < friends.length; i++) {
+		array.push(`Thank you, ${friends[i]}, for the wonderful ${surprise} gift!`);
+	}
+	return array;
+}
+
+function countDown(number) {
+	let i = number;
+	while (i >= 0) {
+		console.log(i);
+		i--
+	}
+}
